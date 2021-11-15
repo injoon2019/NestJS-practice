e82aab52eb5d3c475c82fff6940590e818ea8a0d
Joon <joon@Joonui-MacBookPro.local>
2021-11-15 23:44:34 +0900

    회원 가입 서비스 개발 & DTO 패턴

--- src/cats/cats.schema.ts
+++ src/cats/cats.schema.ts
@@ -31,7 +31,18 @@ export class Cat extends Document {
   password: string;
 
   @Prop()
+  @IsString()
   imgUrl: string;
+
+  readonly readOnlyData: { id: string; email: string; name: string };
 }
 
 export const CatSchema = SchemaFactory.createForClass(Cat);
+
+CatSchema.virtual('readOnlyData').get(function (this: Cat) {
+  return {
+    id: this.id,
+    email: this.email,
+    name: this.name,
+  };
+});
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -14,7 +14,7 @@ export class CatsService {
     const isCatExist = await this.catModel.exists({ email });
 
     if (isCatExist) {
-      throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다');
+      throw new UnauthorizedException('해당하는 고양이는 이미 존재합니다.');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -22,9 +22,9 @@ export class CatsService {
     const cat = await this.catModel.create({
       email,
       name,
-      passowrd: hashedPassword,
+      password: hashedPassword,
     });
 
-    return cat;
+    return cat.readOnlyData;
   }
 }
